79795eaa3d2c3cbe5e320bf347f13505d1264438
Anna Sun <asun4@ualberta.ca>
2023-11-26 13:59:24 -0700

    Initial Commit

---
+++ Enactus Project/Enactus_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enactus_ProjectApp.swift
+//  Enactus Project
+//
+//  Created by Anna on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Enactus_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
